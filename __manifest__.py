03170b69df497fad1b751ad2c17cabb4b4484e3f
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2024-09-02 17:51:28 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'student_management',
+    'version': '1.0.1',
+    'category': 'Education',
+    'sequence': 15,
+    'summary': 'Manage Student Record',
+    'website': 'https://www.est-computer.com/'
+}
